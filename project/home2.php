30177686db7071c788bfc2de7184b014619d69d7
darkmoonpt <rmns95@gmail.com>
2017-01-09 00:37:19 +0000

    algumas alteracoes

--- project/home2.php
+++ project/home2.php
@@ -1,112 +1,5 @@
-<?php require "inc/head.php";
+<form id="filter" name="filter" method="POST" action="<?php echo $current_file; ?>">
 
-if(!isset($_SESSION['id'])) {
-    echo "Não tes acesso a esta pagina";
-    die();
-}
-?>
-<title>Takemore.com - Home</title>
-<?php require "inc/header.php"; ?>
-<?php require "inc/menu.php"; ?>
-<script>
-    var url = window.location.toString();
-    var query_string = url.split('?');
-    var params = query_string[1].split('&');
-    var param_item = params[0].split('=');
-    var imp = param_item[1];
-
-    if(imp == '0'){
-        var params = query_string[1].split('&');
-        var param_item = params[1].split('=');
-        var id = param_item[1];
-        var params = query_string[1].split('&');
-        var param_item = params[2].split('=');
-        var idcli = param_item[1];
-        window.open('print_technical.php?id='+id+'&idcli='+idcli,'_blank')
-    }
-    if(imp == '1'){
-        var params = query_string[1].split('&');
-        var param_item = params[1].split('=');
-        var id = param_item[1];
-        var params = query_string[1].split('&');
-        var param_item = params[2].split('=');
-        var idcli = param_item[1];
-        var params = query_string[1].split('&');
-        var param_item = params[3].split('=');
-        var empr = param_item[1];
-        window.open('print_bill.php?id='+id+'&idcli='+idcli+'&empr='+empr,'_blank')
-    }
-</script>
-<div id="content">
-  <div class="titlecontent">
-  </div>
-  <div class="bodycontent">
-    <form id="filter" name="filter" method="POST" action="<?php echo $current_file; ?>">
-      <fieldset>
-        <legend>filter</legend>
-        <table width="300" border="0">
-          <tr>
-            <td width="70"><label for="Client">Client</label></td>
-            <td width="173">
-           <select name="client" id="client">
-                <?php
-                echo '<option value="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp- See All -</option>';
-                echo '<option ';?><?php active('priv', $_POST['client'])?><?php echo ' value="priv">Private</option>';
-                $SQL = mysqli_query($conn, "SELECT * FROM `client` WHERE `private`='0' ORDER BY `name` ASC");
-if(mysqli_num_rows($SQL)>=1) {
-    while($field = mysqli_fetch_assoc($SQL)){
-        echo '<option ';?><?php active($field['id_client'], $_POST['client'])?><?php echo ' value='.$field['id_client'].'>'.$field['name'].'</option>';
-    }
-}else{
-    echo '<option value="0">No value found</option>';
-}
-                ?>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td><label for="Status">Status</label></td>
-            <td>
-            <select name="status" id="status">
-               <option <?php active('0', $_POST['status'])?> value='0'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp- See All -</option>
-               <option <?php active('Waits', $_POST['status'])?> value="Waits">Waits</option>
-               <option <?php active('Budgeted', $_POST['status'])?> value="Budgeted">Budgeted</option>
-               <option <?php active('Under Repair', $_POST['status'])?> value="Under Repair">Under Repair</option>
-               <option <?php active('Closed Billing', $_POST['status'])?> value="Closed Billing">Closed Billing</option>
-               <option <?php active('Closed Guaranty', $_POST['status'])?> value="Closed Guaranty">Closed Guaranty</option>
-               <option <?php active('Closed Contract', $_POST['status'])?> value="Closed Contract">Closed Contract</option>
-               <option <?php active('Archive', $_POST['status'])?> value="Archive">Archive</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td><label for="Employee">Employee</label></td>
-            <td>
-              <select name="employee" id="employee">
-                <?php
-                echo '<option value="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp- See All -</option>';
-                $SQL = mysqli_query($conn, "SELECT * FROM `users` ORDER BY `name` ASC");
-                if(mysqli_num_rows($SQL)>=1) {
-                    while($field = mysqli_fetch_assoc($SQL)){
-                        echo '<option ';?><?php active($field['id_user'], $_POST['employee'])?><?php echo ' value='.$field['id_user'].'>'.$field['name'].'</option>';
-                    }
-                }else{
-                    echo '<option value="0">No value found</option>';
-                }
-                ?>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td><label for="Entity">Entity</label></td>
-            <td>
-            <select name="entity" id="Entity">
-              <option value="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp- See All -</option>
-              <option <?php active('1', $_POST['entity'])?> value="1">External</option>
-              <option <?php active('2', $_POST['entity'])?> value="2">Internal</option>
-            </select>
-            </td>
-          </tr>
           <tr>
               <td><label for="number">Number ID</label></td>
             <td><input size="4" type="text" name="number" id="NºID" /></td>
@@ -117,6 +10,19 @@ if(mysqli_num_rows($SQL)>=1) {
           </tr>
         </table>
       </fieldset>
+
+
+
+
+
+
+
+
+
+
+
+
+
       <p>&nbsp;</p>
       <table align="center" name="list" id="list">
         <tr>
@@ -182,7 +88,7 @@ if(mysqli_num_rows($SQL)>=1) {
                 $query = mysqli_query($conn, $strCount);
                 $row = mysqli_fetch_array($query);
                 $total = $row["num_registros"];
-                $sql = mysqli_query($conn, "SELECT $campos_query $final_query LIMIT $inicio,$maximo") or die(mysqli_error());
+                $sql = mysqli_query($conn, "SELECT $campos_query $final_query LIMIT $inicio,$maximo") or die("Error:".mysqli_error($conn));
         while ($result_SQL = mysqli_fetch_assoc($sql)) {
             echo '<tr>';
             echo '<td >'.$result_SQL['idd'].'</td>';
